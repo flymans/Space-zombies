00075179c7b80553a23d7ea6c1bd006fad9d9f7f
Aleksander Panteleev <746590@mail.ru>
2018-06-27 23:22:44 +0300

    Linked jquery files

---
+++ Js/SZ_main.js
@@ -0,0 +1,13 @@
+//global vars
+  //need to store the ratio
+    var ratio;
+  //need easy access to the width
+    var newWidth;
+//function that gets called when game starts
+$(window).load(function(){
+  //need to grab an instance of our screen
+  var div = $(window);
+  //we can now work out the ratio
+  ratio = (div.width()/1024);
+  //while
+})
