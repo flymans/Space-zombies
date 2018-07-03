d542db8c936ee447d171c1810fe759cad4fb6c78
Aleksander Panteleev <746590@mail.ru>
2018-07-03 12:40:19 +0300

    fireGun function and minor fixes in css

--- Js/SZ_touch.js
+++ Js/SZ_touch.js
@@ -1,8 +1,3 @@
-//this function is called to reload our Gun
-function reloadGun(e){
-  //play the reload animation of our SS
-  $("#SZ0_1").animateSprite("play", "reload");
-}
 //We need a flag to keep track to avoid repetition of animations before the
 //first has finished
 var canIclick = 0;
@@ -12,7 +7,17 @@ function reloadGun(e){
   //Let's check if we can allow this to occur
   if(canIclick==0){
     //looks like we can so we better set our flag
-    canIclick=1;
+    canIclick = 1;
     $("#SZ0_1").animateSprite("play","reload");
   }
+}
+
+//this function is called to fire our gun
+function fireGun(e) {
+  //Let's check if we can allow this to occur
+    if(canIclick==0){
+      //looks like we can so we beter set our flag
+      canIclick = 1;
+      $("#SZ0_1").animateSprite("play", "fire");
+    }
 }
