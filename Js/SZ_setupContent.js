cdb9f6dccddb1622f1e60f1f72a8ac6eaf418c54
Aleksander Panteleev <746590@mail.ru>
2018-07-06 00:03:57 +0300

    Resetting zombies
    Need to fix problem with moving zombies to one and only X point

--- Js/SZ_setupContent.js
+++ Js/SZ_setupContent.js
@@ -4,21 +4,24 @@
     //first we set their normal sizes in CSS
 
     //Gun
-    $('#SZ0_1').css('width', 150*ratio);
+    $('#SZ0_1').css('width', 150 * ratio);
     $('#SZ0_1').css('height', 150 * ratio);
 
     //Reload Button
-    $('#SZ0_2').css('width', 200*ratio);
+    $('#SZ0_2').css('width', 200 * ratio);
     $('#SZ0_2').css('height', 90 * ratio);
 
     //Score
-    $('#SZ0_3').css('width', 235*ratio);
+    $('#SZ0_3').css('width', 235 * ratio);
     $('#SZ0_3').css('height', 100 * ratio)
 
     //Any sprite sheets?
       //Our Gun
         setup_gun_SS();
 
-    //Create a zombie
-    SZ_createZombie(1);
+    //Create all our 6 zombies
+    for (i = 1; i <7; i++){
+      //this will get called 6 times
+      SZ_createZombie(i);
+    }
   }
--- Js/SZ_zombie_movement.js
+++ Js/SZ_zombie_movement.js
@@ -28,13 +28,15 @@ function SZ_animateZombie(whichOne){
   var timex = [13000,8000,16000,14000,10000,18000];
   //assign a user friendly name for our div
   var $zombiex = $("#zombie"+whichOne);
+  //reset the zombies scale value
+  $zombiex.css('transform', 'scale('+0+')');
   //work out the amount the zombie has to come towards us
   var amty = ($(window).height()*0.7); // -($zombiex.height()*2); //topx);
   //each type of zombie will have their own walking style
   var SZ_ease = ['easeInSine','easeOutQuart','easeInOutQuad','easeInSine',
   'easeOutQuart','easeInOutQuad'];
   //finally we are ready to animate
-    $zombiex.animate({
+    $zombiex.delay(timex[whichOne-1]/3).animate({
       //first bring our zombie slowly down the screen
       left: amty+ "px",},
       {
@@ -45,11 +47,36 @@ function SZ_animateZombie(whichOne){
           if (fx.prop == "left") {
             //work out the amount to scale
             var xx = (fx.pos)*16;
+          //do a check to see if we should end this animation
+            if(xx>15){
+              //stop all animation
+              $(this).stop();
+              //call a function to reset this zombie
+              SZ_resetZombie(whichOne);
+            } else {
             //apply the scale
             $(this).css('transform','scale('+xx+')');
           }
+        }
         },
         complete: function() {
       }
     });
+}
+//a function to completely reset our zombie
+function SZ_resetZombie(whichOne){
+  //assign a user friendly name for our div
+  var $zombiex = $("#zombie"+whichOne);
+  //we want to position our zombie exactly at the tip of the planet
+  var top_position = $('#SZ0_0').height()*0.435;
+  //Xpos can be anywhere on our x axis
+  var left_position = Math.floor(Math.random()*($('#SZ0_0').width()-ratio*50))+
+  (ratio*50);
+  //let's re-position our zombie
+  $zombiex.css({
+    top: top_position+'px',
+    left: left_position='px'
+  });
+  //finally let's make the zombie come towards the screen again
+  SZ_animateZombie(whichOne);
 }
