e48d89b1cca4a42b75bed9c46947dfa45d193947
Aleksander Panteleev <746590@mail.ru>
2018-07-05 16:49:11 +0300

    Added zombie animation

--- Js/SZ_zombie_movement.js
+++ Js/SZ_zombie_movement.js
@@ -18,4 +18,38 @@ function SZ_createZombie(whichOne){
   document.body.appendChild(div);
   //put this new zombie through our SS function
   setup_zombie_SS(whichOne);
+  //put this new zombie through our animate function
+  SZ_animateZombie(whichOne);
+}
+
+//let's animate our zombie toward us
+function SZ_animateZombie(whichOne){
+  //assign the speed for each of our zombies
+  var timex = [13000,8000,16000,14000,10000,18000];
+  //assign a user friendly name for our div
+  var $zombiex = $("#zombie"+whichOne);
+  //work out the amount the zombie has to come towards us
+  var amty = ($(window).height()*0.7); // -($zombiex.height()*2); //topx);
+  //each type of zombie will have their own walking style
+  var SZ_ease = ['easeInSine','easeOutQuart','easeInOutQuad','easeInSine',
+  'easeOutQuart','easeInOutQuad'];
+  //finally we are ready to animate
+    $zombiex.animate({
+      //first bring our zombie slowly down the screen
+      left: amty+ "px",},
+      {
+        easing:SZ_ease[whichOne-1],
+        duration: timex[whichOne-1],
+        step: function(now, fx){
+          //at each step we can manipulate the scale of our zombie
+          if (fx.prop == "left") {
+            //work out the amount to scale
+            var xx = (fx.pos)*16;
+            //apply the scale
+            $(this).css('transform','scale('+xx+')');
+          }
+        },
+        complete: function() {
+      }
+    });
 }
